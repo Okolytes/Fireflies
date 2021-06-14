06f50b38b0a9a110edad2cc6dcff63bf394ac490
lerrific <43840962+lerrific@users.noreply.github.com>
2021-06-15 08:38:35 +1000

    couple of tweaks and fixes

--- src/main/java/fireflies/Fireflies.java
+++ src/main/java/fireflies/Fireflies.java
@@ -5,18 +5,18 @@ import fireflies.client.particle.FireflyDustParticle;
 import fireflies.client.render.FireflyRenderer;
 import fireflies.client.render.GlassJarRenderer;
 import fireflies.entity.FireflyEntity;
+import fireflies.misc.FireflyMasterPotion;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.item.crafting.Ingredient;
-import net.minecraft.potion.PotionUtils;
 import net.minecraft.potion.Potions;
 import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -35,7 +35,7 @@ public class Fireflies {
 
     public Fireflies() {
         final IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        Registry.init(bus);
+        Registry.register(bus);
         bus.register(this);
     }
 
@@ -46,18 +46,24 @@ public class Fireflies {
 
     @SubscribeEvent
     public void commonSetup(final FMLCommonSetupEvent event) {
-        BrewingRecipeRegistry.addRecipe(Ingredient.fromStacks(PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Potions.AWKWARD)), Ingredient.fromItems(Registry.ILLUMERIN.get()), PotionUtils.addPotionToItemStack(new ItemStack(Items.POTION), Registry.FIREFLY_MASTER.get()));
+        event.enqueueWork(() -> {
+            // Splash, lingering, arrows etc get registered automatically.
+            BrewingRecipeRegistry.addRecipe(new FireflyMasterPotion.FireflyMasterBrewingRecipe(Potions.AWKWARD, new ItemStack(Registry.ILLUMERIN.get()), Registry.FIREFLY_MASTER.get()));
+            BrewingRecipeRegistry.addRecipe(new FireflyMasterPotion.FireflyMasterBrewingRecipe(Registry.FIREFLY_MASTER.get(), new ItemStack(Items.REDSTONE), Registry.LONG_FIREFLY_MASTER.get()));
+            BrewingRecipeRegistry.addRecipe(new FireflyMasterPotion.FireflyMasterBrewingRecipe(Registry.FIREFLY_MASTER.get(), new ItemStack(Items.GLOWSTONE_DUST), Registry.STRONG_FIREFLY_MASTER.get()));
+        });
     }
 
     @SubscribeEvent
     public void clientSetup(final FMLClientSetupEvent event) {
-        RenderTypeLookup.setRenderLayer(Registry.GLASS_JAR.get(), RenderType.getCutout());
+        event.enqueueWork(() -> RenderTypeLookup.setRenderLayer(Registry.GLASS_JAR.get(), RenderType.getCutout()));
         ClientRegistry.bindTileEntityRenderer(Registry.GLASS_JAR_TILE_ENTITY.get(), GlassJarRenderer::new);
         RenderingRegistry.registerEntityRenderingHandler(Registry.FIREFLY.get(), FireflyRenderer::new);
     }
 
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     public void registerParticles(final ParticleFactoryRegisterEvent event) {
+        // Called only on the client.
         ParticleManager particleManager = Minecraft.getInstance().particles;
         particleManager.registerFactory(Registry.FIREFLY_DUST_PARTICLE.get(), FireflyDustParticle.DustParticleFactory::new);
         particleManager.registerFactory(Registry.FIREFLY_DUST_REDSTONE_PARTICLE.get(), FireflyDustParticle.DustRedstoneParticleFactory::new);
