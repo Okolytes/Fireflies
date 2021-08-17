35e67b291053f7f62b4c07eed10436cd2cd8098a
lerrific <43840962+lerrific@users.noreply.github.com>
2021-08-17 18:11:08 +1000

    Revert "Revert "he;lp""
    This reverts commit 9a4876dc0eb538c7a74a8313eed46d736a343dd1.

--- src/main/java/fireflies/misc/GlassJarFluid.java
+++ src/main/java/fireflies/misc/GlassJarFluid.java
@@ -1,5 +1,6 @@
 package fireflies.misc;
 
+import fireflies.Fireflies;
 import net.minecraft.fluid.EmptyFluid;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.FluidAttributes;
@@ -12,18 +13,22 @@ public class GlassJarFluid extends EmptyFluid {
     private final FluidStack fluidStack;
     private final int volume;
     private final int color;
-    private final int luminosity;
+    private final byte luminosity;
 
     public GlassJarFluid(String textureLocation, int volume) {
-        this(textureLocation, volume, 0xffffffff, 0);
+        this(textureLocation, volume, 0xffffffff, (byte)0);
     }
 
     public GlassJarFluid(String textureLocation, int volume, int color) {
-        this(textureLocation, volume, color, 0);
+        this(textureLocation, volume, color, (byte) 0);
     }
 
-    public GlassJarFluid(String textureLocation, int volume, int color, int luminosity) {
-        this.textureLocation = textureLocation;
+    public GlassJarFluid(String textureLocation, int volume, byte luminosity) {
+        this(textureLocation, volume, 0xffffffff, luminosity);
+    }
+
+    public GlassJarFluid(String textureLocation, int volume, int color, byte luminosity) {
+        this.textureLocation = String.format("%s:block/%s", Fireflies.MOD_ID, textureLocation);
         this.volume = volume;
         this.color = color;
         this.fluidStack = new FluidStack(this, volume);
@@ -40,6 +45,6 @@ public class GlassJarFluid extends EmptyFluid {
 
     @Override
     protected FluidAttributes createAttributes() {
-        return FluidAttributes.builder(new ResourceLocation(textureLocation), new ResourceLocation(textureLocation)).color(color).luminosity(luminosity).build(this);
+        return FluidAttributes.builder(new ResourceLocation(textureLocation + "_side"), new ResourceLocation(textureLocation + "_top")).color(color).luminosity(luminosity).build(this);
     }
 }
