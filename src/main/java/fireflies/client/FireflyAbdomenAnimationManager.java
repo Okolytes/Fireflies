d0c2b6c32db259323311d0c8cd663678e89f4f93
lerrific <43840962+lerrific@users.noreply.github.com>
2022-05-17 06:20:42 +1000

    simplify setAnimation

--- src/main/java/fireflies/client/FireflyAbdomenAnimationManager.java
+++ src/main/java/fireflies/client/FireflyAbdomenAnimationManager.java
@@ -10,13 +10,12 @@ import net.minecraftforge.fml.common.Mod;
 
 import javax.annotation.Nullable;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Objects;
 
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = Fireflies.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class FireflyAbdomenAnimationManager {
-    public static final HashSet<FireflyAbdomenAnimation> ANIMATIONS = new HashSet<>();
+    public static final HashMap<String, FireflyAbdomenAnimation> ANIMATIONS = new HashMap<>();
     public static final HashMap<String, FireflyEntity> WANTS_IN = new HashMap<>();
     public static final HashMap<String, FireflyEntity> WANTS_OUT = new HashMap<>();
     public final FireflyAbdomenAnimationProperties animationProperties = new FireflyAbdomenAnimationProperties();
@@ -33,40 +32,24 @@ public class FireflyAbdomenAnimationManager {
     @SubscribeEvent
     public static void tickAnimations(TickEvent.ClientTickEvent event) {
         if (event.phase == TickEvent.Phase.START /* Runs on both START and END */ && !Minecraft.getInstance().isGamePaused() && Minecraft.getInstance().player != null) {
-            ANIMATIONS.forEach(FireflyAbdomenAnimation::tick);
+            ANIMATIONS.values().forEach(FireflyAbdomenAnimation::tick);
         }
     }
 
     /**
-     * @param name null = no animation
+     * @param newAnimation null = no animation
      */
-    public void setAnimation(@Nullable String name) {
-        if (Objects.equals(name, this.curAnimation)) {
+    public void setAnimation(@Nullable String newAnimation) {
+        if (Objects.equals(newAnimation, this.curAnimation)) {
             return;
         }
 
-        boolean flag = false;
-        for (FireflyAbdomenAnimation animation : ANIMATIONS) {
-            final boolean contains = animation.fireflies.contains(this.firefly);
-            if (animation.name.equals(name) && !contains) {
-                WANTS_IN.put(animation.name, this.firefly);
-                this.prevAnimation = this.curAnimation;
-                this.curAnimation = name;
-                this.resetAnimationProperties();
-                // No need to continue iterating
-                if (flag) {
-                    return;
-                }
-            } else if (contains) {
-                flag = true;
-                WANTS_OUT.put(animation.name, this.firefly);
-                if (name == null) {
-                    this.prevAnimation = this.curAnimation;
-                    this.curAnimation = null;
-                    this.resetAnimationProperties();
-                    return;
-                }
-            }
+        this.prevAnimation = this.curAnimation;
+        this.curAnimation = newAnimation;
+        this.resetAnimationProperties();
+        WANTS_OUT.put(this.curAnimation, this.firefly);
+        if (newAnimation != null) {
+            WANTS_IN.put(newAnimation, this.firefly);
         }
     }
 
