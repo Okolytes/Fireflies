42bcf2ec4cc0e930a33240deb339471319ec0107
lerrific <43840962+lerrific@users.noreply.github.com>
2022-06-03 09:19:19 +1000

    update abdomen particle position

--- src/main/java/fireflies/client/particle/FireflyParticleManager.java
+++ src/main/java/fireflies/client/particle/FireflyParticleManager.java
@@ -37,9 +37,9 @@ public class FireflyParticleManager {
      */
     public double[] getAbdomenParticlePos() {
         return new double[] {
-                this.firefly.getPosX() - -this.firefly.getWidth() * 0.25f * MathHelper.sin(this.firefly.renderYawOffset * ((float) Math.PI / 180F)),
+                this.firefly.getPosX() - -this.firefly.getWidth() * 0.5f * MathHelper.sin(this.firefly.renderYawOffset * ((float) Math.PI / 180F)),
                 this.firefly.getPosYEye() + this.abdomenParticlePositionOffset + (this.firefly.hasIllumerin() ? 0.6f : 0.3f),
-                this.firefly.getPosZ() + -this.firefly.getWidth() * 0.25f * MathHelper.cos(this.firefly.renderYawOffset * ((float) Math.PI / 180F))
+                this.firefly.getPosZ() + -this.firefly.getWidth() * 0.5f * MathHelper.cos(this.firefly.renderYawOffset * ((float) Math.PI / 180F))
         };
     }
 
